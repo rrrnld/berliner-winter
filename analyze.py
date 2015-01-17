309cd191af7ee612a14e0fe893d3c0f0b9bdd2df
Joshua Widmann <joshuawidmann@gmx.de>
2015-01-17 17:37:18 +0100

    Function to get longitude and latitude from certain query to OSM.

--- analyze.py
+++ analyze.py
@@ -1,3 +1,5 @@
+import requests, json
+
 def get_district(article_headline):
     """
     Returns a geo-coded version of a district an article is about, based on its
@@ -23,3 +25,10 @@ def get_categories(article_body):
     found_categories = [bad_words[key] for key in bad_words
                                         if key in article_body.lower()]
     return found_categories or ['other']
+
+def get_geoloc(query):
+    query.replace(" ", "%20")
+    url = "http://nominatim.openstreetmap.org/search?q=" + query + "%20Berlin" + "&countrycodes=de&format=json&limit=1"
+    r = requests.get(url)
+
+    return(r.json()[0]["lat"], r.json()[0]["lon"])
