705472c9dc252b29d6990e50ab6885205741d93d
Arne Schlüter <aesthaddicts@gmail.com>
2015-02-07 17:37:27 +0100

    Add more server APIs

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
 import bottle
-from json import dumps
 from models import *
 
-@bottle.get('/locations/<article_id:int>')
+@bottle.get("/locations/<article_id:int>")
 def location(article_id):
     return (Location
         .select()
@@ -11,5 +10,26 @@ def location(article_id):
         .dicts()
         .get())
 
-if __name__ == '__main__':
-    bottle.run(host='localhost', port=12345, reloader=True, debug=True)
+@bottle.get("/categories/<article_id:int>")
+def category(article_id):
+    categories = (Category
+        .select()
+        .where(Category.article == article_id))
+
+    return {
+        "article": article_id,
+        "categories": [c.name for c in categories]
+    }
+
+@bottle.get("/articles/<article_id:int>")
+def article(article_id):
+    # articles have a datetime field and need special serialization
+    # return json_util.dumps(Article
+    #     .select()
+    #     .where(Article.id == article_id)
+    #     .dicts()
+    #     .get())
+    pass
+
+if __name__ == "__main__":
+    bottle.run(host="localhost", port=12345, reloader=True, debug=True)
